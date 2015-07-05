5dfa9baae87de699986dd429438aadcd08ecf07f
thomas.wilding <thomas.wilding@accenture.com>
2015-07-05 01:58:52 +0100

    Initial Commit

---
+++ mouseMove/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mouseMove
+//
+//  Created by Tom Wilding on 05/07/2015.
+//  Copyright (c) 2015 Tom Wilding. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mouseMove/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mouseMove
+//
+//  Created by Tom Wilding on 05/07/2015.
+//  Copyright (c) 2015 Tom Wilding. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
